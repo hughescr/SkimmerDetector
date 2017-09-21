c27180d70f937383a0295c3094ba159b6a69f77a
Craig R. Hughes <craig.git@rungie.com>
2017-09-20 20:59:20 -0700

    Initial Commit

---
+++ SkimmerDetector/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SkimmerDetector
+//
+//  Created by Craig Hughes on 9/20/17.
+//  Copyright © 2017 Rungie.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
